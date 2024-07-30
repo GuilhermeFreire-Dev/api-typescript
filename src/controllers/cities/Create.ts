2aea4308e660348cf2bfa026fc21c62e351c86a1
Guilherme Freire <guilherme.freire@novomundo.com.br>
2024-07-30 14:12:37 -0300

    :sparkles: feat: add validation

--- src/controllers/cities/Create.ts
+++ src/controllers/cities/Create.ts
@@ -1,11 +1,26 @@
 import { Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
+import * as yup from "yup";
 
 interface CreateCityInterface {
   name: string
 }
 
-export const create = (req: Request<{}, {}, CreateCityInterface>, res: Response) => {
-  console.log(req.body.name);
+const validation: yup.Schema<CreateCityInterface> = yup.object().shape({
+  name: yup.string().required().min(3)
+});
+
+export const create = async(req: Request<{}, {}, CreateCityInterface>, res: Response) => {
+  let validatedData: CreateCityInterface | undefined = undefined;
+
+  try {
+    validatedData = await validation.validate(req.body);
+  } catch (error) {
+    const validationError = error as yup.ValidationError;
+    return res.status(StatusCodes.BAD_REQUEST).json({
+      errors: validationError.message
+    });
+  }
   
-  return res.send("create");
+  return res.json(validatedData);
 }
